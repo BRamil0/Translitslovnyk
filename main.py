941339f91fe092b310498334336d9ec0105a513b
Radomyr <qulowg@gmail.com>
2025-06-02 14:50:35 +0300

    feat: Додано головний файл програми з асинхронним завантаженням словника та транслітерацією тексту

--- main.py
+++ main.py
@@ -1,14 +1,22 @@
 """
-
+Головний файл програми
 """
 import asyncio
 from source.logger import logger
+from source.dictionary import Dictionary
+from source.translate import Translate
+
 
 async def main() -> None:
     """
 
     :return:
     """
+    dictionary = Dictionary("test.json")
+    await dictionary.load()
+    translator = Translate(dictionary, input("Текст: "))
+    print(translator.transliterate())
+
     return
 
 if __name__ == "__main__":
