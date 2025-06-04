623f8f106f199f07db682bdac7295856312ac152
Radomyr <qulowg@gmail.com>
2025-06-04 07:49:32 +0300

    feat: Файл logger.py: - Додано фіксацію подій завантаження реєстратора журналу та перевірку, чи увімкнено виведення журналу в консоль.

--- source/logger.py
+++ source/logger.py
@@ -9,5 +9,9 @@ logger.remove()
 if settings.is_log:
     logger.add(settings.LOG_DIR / "info.log", rotation="10 MB", enqueue=True, backtrace=True, diagnose=True, format=settings.LOG_FORMAT, encoding="utf-8")
     logger.add(settings.LOG_DIR / "error.log", level="ERROR", rotation="10 MB", enqueue=True, backtrace=True, diagnose=True, format=settings.LOG_FORMAT, encoding="utf-8")
+    logger.debug(f"(logger) Журналювання ввімкнено, файли журналів зберігаються в: {settings.LOG_DIR}")
     if settings.is_show_log:
+        logger.info("(logger) Виведення журналів на консоль увімкнено.")
         logger.add(sys.stdout, colorize=True, format=settings.LOG_FORMAT)
+    else:
+        logger.info("(logger) Виведення журналів на консоль вимкнено.")
