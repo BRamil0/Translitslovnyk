7638598b2d2cc0d7c421bc24c1e2cb490bb18dde
Radomyr <qulowg@gmail.com>
2025-06-04 07:16:25 +0300

    feat: - Додано опис файлу - Додано модель DictionaryModel клас IODictionary: - Тепер функції read_dictionary та write_dictionary повертають DictionaryModel, а не словник - write_dictionary приймає тепер DictionaryModel замість словника клас Dictionary: - Поле dictionary тепер підтримує лише DictionaryModel - Функції __getitem__, __setitem__, __delitem__ працюють з полем date у моделі DictionaryModel

--- source/dictionary.py
+++ source/dictionary.py
@@ -1,10 +1,19 @@
 """
-
+Файл по роботи з словниками.
 """
 import json
-import aiofiles
 from pathlib import Path
 
+import aiofiles
+import pydantic
+
+
+class DictionaryModel(pydantic.BaseModel):
+    class InfoModel(pydantic.BaseModel):
+        name: str
+
+    data: dict[str, str] | None = None
+    info: InfoModel
 
 class IODictionary:
     path: Path = Path(Path(__file__).parent.parent, "dictionaries")
@@ -13,42 +22,42 @@ class IODictionary:
         if path:
             self.path = path
 
-    async def read_dictionary(self, filename: Path | str, directorate: Path | None = None) -> dict[str, str | dict]:
+    async def read_dictionary(self, filename: Path | str, directorate: Path | None = None) -> DictionaryModel:
         if isinstance(filename, str):
             path: Path = (self.path if directorate is None else directorate) / filename
         else:
             path = filename
         async with aiofiles.open(path, "r", encoding="utf-8") as f:
             content = await f.read()
-            return json.loads(content)
+            return DictionaryModel.model_validate_json(content)
 
-    async def write_dictionary(self, filename: Path | str, dictionary: dict[str, str | dict], directorate: Path | None = None) -> dict[str, str | dict]:
+    async def write_dictionary(self, filename: Path | str, dictionary: DictionaryModel, directorate: Path | None = None) -> DictionaryModel:
         if isinstance(filename, str):
             path: Path = (self.path if directorate is None else directorate) / filename
         else:
             path = filename
         async with aiofiles.open(path, "w", encoding="utf-8") as f:
-            await f.write(json.dumps(dictionary, indent=4))
+            await f.write(dictionary.model_dump_json(indent=4, exclude_none=True))
         return dictionary
 
 class Dictionary:
     file: Path
-    dictionary: dict
+    dictionary: DictionaryModel | None
     iod: IODictionary
 
     def __init__(self, file: Path, iod: IODictionary | None = None) -> None:
-        self.dictionary = {}
+        self.dictionary = None
         self.file = file
         self.iod = iod if iod else IODictionary()
 
     def __getitem__(self, key):
-        return self.dictionary.__getitem__(key)
+        return self.dictionary.data.__getitem__(key)
 
     def __setitem__(self, key, value):
-        self.dictionary.__setitem__(key, value)
+        self.dictionary.data.__setitem__(key, value)
 
     def __delitem__(self, key):
-        self.dictionary.__delitem__(key)
+        self.dictionary.data.__delitem__(key)
 
     async def load(self) -> bool:
         try:
