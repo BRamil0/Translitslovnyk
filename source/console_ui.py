34a4a334f5f2aeff57676d6fbf0761b23c1d79a0
Radomyr <qulowg@gmail.com>
2025-06-06 07:54:01 +0300

    feat: Додано файл console_ui.py та клас ConsoleUI для ІК (Інтерфейсу Користувача), поки що має тільки базові функції.

---
+++ source/console_ui.py
@@ -0,0 +1,32 @@
+"""
+Інтерфейс користувача для консолі.
+"""
+
+class ConsoleUI:
+    """
+    Клас для взаємодії з користувачем через консоль.
+    """
+
+    def __init__(self) -> None:
+        pass
+
+    @staticmethod
+    async def display_message(self, message: str) -> None:
+        """
+        Відображає повідомлення в консолі.
+
+        :param message: Повідомлення для відображення.
+        """
+        print(message)
+
+    @staticmethod
+    async def get_input(self, prompt: str) -> str:
+        """
+        Отримує вхідні дані від користувача.
+
+        :param prompt: Запит для користувача.
+        :return: Введені дані.
+        """
+        return input(prompt)
+
+cui: ConsoleUI = ConsoleUI()
