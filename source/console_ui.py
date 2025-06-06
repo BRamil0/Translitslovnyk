e0055d9892c6acb3d6dfd6349d8093d762656874
Radomyr <qulowg@gmail.com>
2025-06-06 08:57:08 +0300

    feat: Додано display_dictionary_list для зручного виводу списку з DictionaryManager

--- source/console_ui.py
+++ source/console_ui.py
@@ -2,6 +2,10 @@
 Інтерфейс користувача для консолі.
 """
 
+from source.dictionary import Dictionary, DictionaryManager
+from source.internationalization import i18n
+
+
 class ConsoleUI:
     """
     Клас для взаємодії з користувачем через консоль.
@@ -29,4 +33,16 @@ class ConsoleUI:
         """
         return input(prompt)
 
+    @staticmethod
+    async def display_dictionary_list(dictionary_manager: DictionaryManager) -> None:
+        """
+        Відображає список словників.
+
+        :param dictionary_manager: Менеджер словників.
+        """
+        await cui.display_message(i18n["list_dictionaries"])
+        for key in dictionary_manager.get_list_dictionaries():
+            await cui.display_message(f"{dictionary_manager[key].dictionary.info.name} - {key}")
+
 cui: ConsoleUI = ConsoleUI()
+
