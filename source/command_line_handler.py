d08bc7af2d14b2a84ea92e64993400a4477bc69a
Radomyr <qulowg@gmail.com>
2025-06-07 09:34:41 +0300

    feat: Додано файл command_line_handler.py для парсингу аргументів командного рядка

---
+++ source/command_line_handler.py
@@ -0,0 +1,38 @@
+"""
+Модуль для обробки аргументів командного рядка.
+"""
+
+import argparse
+
+from source.logger import logger
+
+async def add_parser_arguments(parser: argparse.ArgumentParser) -> None:
+    """
+    Додає аргументи командного рядка до парсера.
+
+    :param parser: Парсер аргументів командного рядка.
+    """
+    parser.add_argument("-d", "--dictionary", required=False)
+    parser.add_argument("-t", "--text", required=False)
+    parser.add_argument("-i", "--input", required=False)
+    parser.add_argument("-o", "--output", required=False)
+
+async def parse_command_line_arguments() -> argparse.Namespace:
+    """
+    Парсить аргументи командного рядка.
+
+    :return: Простір імен з аргументами командного рядка.
+    """
+
+    parser = argparse.ArgumentParser(description="Транслітерація тексту за словником.")
+    await add_parser_arguments(parser)
+    args = parser.parse_args()
+
+    if all(value is None for value in vars(args).values()):
+        return args
+
+    if not args.dictionary or (not args.text and not args.input):
+        logger.error("Необхідно вказати словник та текст або файл для транслітерації.")
+        parser.error("Необхідно вказати словник та текст або файл для транслітерації.")
+
+    return args
