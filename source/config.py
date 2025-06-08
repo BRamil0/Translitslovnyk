e00e14c7787b37bef0cb5c86a6ff9e25cbca018d
Radomyr <qulowg@gmail.com>
2025-06-08 10:55:58 +0300

    feat!: Додано поле version, PATH_JSON_FILE_SETTINGS та функцію safe_settings. Змінено назву поля LOG_DIR на PATH_LOG_DIR

--- source/config.py
+++ source/config.py
@@ -4,21 +4,26 @@
 import pydantic_settings
 from pathlib import Path
 
+import aiofiles
+
 class Settings(pydantic_settings.BaseSettings):
     """Клас налаштувань програми."""
 
     language: str = "ukr"
+    version: str = "1.0.0"
 
     is_log: bool = True
     is_show_log: bool = False
-    LOG_DIR: Path = Path(__file__).parent.parent / "temp" / "logs"
     LOG_FORMAT: str = "<y>IDP:{process}</y> <ly>SPT:{elapsed}</ly> | <g>{time:YYYY-MM-DD}</g> <lg>{time:HH:mm:ss}</lg> | <level>{level}</level> | <m>F:{file}</m> <lm>L:{line} FU:{function}</lm> | {message}"
 
     path_dictionaries: Path = Path(__file__).parent.parent / "dictionaries"
-    path_temp: Path = Path(__file__).parent.parent / "temp"
     path_internationalization: Path = Path(__file__).parent.parent / "internationalization"
 
-    model_config = pydantic_settings.SettingsConfigDict(json_file=(Path(__file__).parent.parent / "config.json"))
+    PATH_LOG_DIR: Path = Path(__file__).parent.parent / "temp" / "logs"
+    PATH_TEMP: Path = Path(__file__).parent.parent / "temp"
+    PATH_JSON_FILE_SETTINGS = Path(__file__).parent.parent / "config.json"
+
+    model_config = pydantic_settings.SettingsConfigDict(json_file=PATH_JSON_FILE_SETTINGS)
 
     @classmethod
     def settings_customise_sources(
@@ -30,5 +35,12 @@ class Settings(pydantic_settings.BaseSettings):
             file_secret_settings: pydantic_settings.PydanticBaseSettingsSource,
     ) -> tuple[pydantic_settings.PydanticBaseSettingsSource, ...]:
         return (pydantic_settings.JsonConfigSettingsSource(settings_cls),)
+
+    async def safe_settings(self) -> None:
+        """
+        Асинхронний метод для безпечного збереження налаштувань.
+        """
+        with await aiofiles.open(self.PATH_JSON_FILE_SETTINGS, mode='w', encoding='utf-8') as f:
+            await f.write(self.model_dump_json(indent=4, exclude_none=True))
 
 settings: Settings = Settings()
--- source/logger.py
+++ source/logger.py
@@ -10,9 +10,9 @@ from source.config import settings
 logger.remove()
 
 if settings.is_log:
-    logger.add(settings.LOG_DIR / "info.log", rotation="10 MB", enqueue=True, backtrace=True, diagnose=True, format=settings.LOG_FORMAT, encoding="utf-8")
-    logger.add(settings.LOG_DIR / "error.log", level="ERROR", rotation="10 MB", enqueue=True, backtrace=True, diagnose=True, format=settings.LOG_FORMAT, encoding="utf-8")
-    logger.debug(f"(logger) Журналювання ввімкнено, файли журналів зберігаються в: {settings.LOG_DIR}")
+    logger.add(settings.PATH_LOG_DIR / "info.log", rotation="10 MB", enqueue=True, backtrace=True, diagnose=True, format=settings.LOG_FORMAT, encoding="utf-8")
+    logger.add(settings.PATH_LOG_DIR / "error.log", level="ERROR", rotation="10 MB", enqueue=True, backtrace=True, diagnose=True, format=settings.LOG_FORMAT, encoding="utf-8")
+    logger.debug(f"(logger) Журналювання ввімкнено, файли журналів зберігаються в: {settings.PATH_LOG_DIR}")
     if settings.is_show_log:
         logger.info("(logger) Виведення журналів на консоль увімкнено.")
         logger.add(sys.stdout, colorize=True, format=settings.LOG_FORMAT)
