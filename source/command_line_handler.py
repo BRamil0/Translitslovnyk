cf8cc4e110269f1b7a69d0bc08315baa602002c0
Radomyr <qulowg@gmail.com>
2025-06-09 14:06:40 +0300

    feat: Тепер опис аргументів підтримує локалізацію.

--- source/command_line_handler.py
+++ source/command_line_handler.py
@@ -6,6 +6,7 @@ import argparse
 from pathlib import Path
 
 from source.logger import logger
+from source.internationalization import i18n
 
 async def add_parser_arguments(parser: argparse.ArgumentParser) -> None:
     """
@@ -13,19 +14,21 @@ async def add_parser_arguments(parser: argparse.ArgumentParser) -> None:
 
     :param parser: Парсер аргументів командного рядка.
     """
-    parser.add_argument("-d", "--dictionary", required=False, type=str, help="Шлях до словника для транслітерації.")
-    parser.add_argument("-t", "--text", required=False, type=str, help="Текст для транслітерації.")
-    parser.add_argument("-i", "--input", required=False, type=Path, help="Шлях до вхідного файлу з текстом для транслітерації.")
-    parser.add_argument("-o", "--output", required=False, type=Path, help="Шлях до вихідного файлу для збереження результатів транслітерації.")
+    parser.add_argument("-h", "--help", action="help", help=i18n["--help_help"])
 
-    parser.add_argument("-v", "--version", required=False, action="store_true", help="Показати версію програми.")
-    parser.add_argument("-a", "--author", required=False, action="store_true", help="Показати інформацію про автора програми.")
-    parser.add_argument("-g", "--github", required=False, action="store_true", help="Показати посилання на репозиторій GitHub програми.")
-    parser.add_argument("-info", "--information", required=False, action="store_true", help="Показати інформацію про програму.")
+    parser.add_argument("-d", "--dictionary", required=False, type=str, help=i18n["--dictionary_help"])
+    parser.add_argument("-t", "--text", required=False, type=str, help=i18n["--text_help"])
+    parser.add_argument("-i", "--input", required=False, type=Path, help=i18n["--input_help"])
+    parser.add_argument("-o", "--output", required=False, type=Path, help=i18n["--output_help"])
 
-    parser.add_argument("-id", "--information_dictionary", required=False, type=str, help="Показати інформацію про словник за його назвою, назвою файлу або ID.")
+    parser.add_argument("-v", "--version", required=False, action="store_true", help=i18n["--version_help"])
+    parser.add_argument("-a", "--author", required=False, action="store_true", help= i18n["--author_help"])
+    parser.add_argument("-g", "--github", required=False, action="store_true", help= i18n["--github_help"])
+    parser.add_argument("-info", "--information", required=False, action="store_true", help= i18n["--information_help"])
 
-    parser.add_argument("-l", "--language", required=False, type=str, help="Мова для інтернаціоналізації.")
+    parser.add_argument("-id", "--information_dictionary", required=False, type=str, help= i18n["--information_dictionary_help"])
+
+    parser.add_argument("-l", "--language", required=False, type=str, help= i18n["--language_help"])
 
 async def parse_command_line_arguments() -> argparse.Namespace:
     """
@@ -34,7 +37,7 @@ async def parse_command_line_arguments() -> argparse.Namespace:
     :return: Простір імен з аргументами командного рядка.
     """
 
-    parser = argparse.ArgumentParser(description="Транслітерація тексту за словником.")
+    parser = argparse.ArgumentParser(description=i18n["description_argparse"], add_help=False)
     await add_parser_arguments(parser)
     args = parser.parse_args()
     logger.debug(f"Було отримано аргументи командного рядка: {args}")
