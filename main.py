34fdfde6a67dfda717c8935885b11c49540fe3fe
Radomyr <qulowg@gmail.com>
2025-06-15 11:57:40 +0300

    feat: - Перейменовано e у блоках except на більш описові назви. - Додано привітальне повідомлення. - Додано обробку аргументу -nw для вимкнення привітального повідомлення.

--- main.py
+++ main.py
@@ -78,26 +78,35 @@ async def main() -> None:
     """
     Головна функція програми.
     """
+    logger.debug("Початок роботи програми.")
     try:
         await internationalization.load_localization()
-    except FileNotFoundError as e:
-        logger.error(f"Файл локалізації не знайдено: {e}. Перевірте наявність файлу в директорії {settings.path_internationalization}. Або змініть config.json, щоб вказати іншу мову.")
-        await cui.display_message("Не вдалося завантажити мову, рекомендується почистити файл config.json")
-        raise e
+    except FileNotFoundError as FNFError:
+        logger.error(
+            f"Файл локалізації не знайдено: {FNFError}. Перевірте наявність файлу в директорії {settings.path_internationalization}. Або змініть config.json, щоб вказати іншу мову."
+        )
+        await cui.display_message(
+            "Не вдалося завантажити мову, рекомендується почистити файл config.json"
+        )
+        raise FNFError
+    logger.debug("Завантаження локалізації завершено.")
 
     dm: DictionaryManager = DictionaryManager()
     await dm.index()
     args: argparse.Namespace = await parse_command_line_arguments()
 
+    if not args.not_welcome:
+        await cui.display_panel(i18n["welcome_message"].format(settings.version, i18n.get_lm().info.name))
+
     if args.language:
         try:
             internationalization.set_language(args.language)
             await internationalization.load_localization()
-        except FileNotFoundError as e:
+        except FileNotFoundError as FNFError:
             internationalization.set_language(settings.language)
             await internationalization.load_localization()
 
-            logger.error(f"Файл локалізації для мови {args.language} не знайдено: {e}.")
+            logger.error(f"Файл локалізації для мови {args.language} не знайдено: {FNFError}.")
             await cui.display_message(i18n["language_file_not_found"].format(args.language))
             return None
         settings.language = args.language
@@ -179,6 +188,6 @@ async def main() -> None:
 if __name__ == "__main__":
     try:
         asyncio.run(main())
-    except (KeyboardInterrupt, EOFError, UnicodeDecodeError) as e:
+    except (KeyboardInterrupt, EOFError, UnicodeDecodeError) as error:
         asyncio.run(cui.display_message("\n" + i18n["transliteration_exiting"]))
-        logger.debug(f"Вихід з програми через помилку: {e}, скоріше за все, це було викликано натисканням Ctrl+C або Ctrl+D.")
+        logger.debug(f"Вихід з програми через помилку: {error}, скоріше за все, це було викликано натисканням Ctrl+C або Ctrl+D.")
